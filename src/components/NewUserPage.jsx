ac2dac0f8c632fb89fe7cd2a20dcd0dc4a934133
William Tucker <william.tucker29@gmail.com>
2024-08-05 13:05:35 -0500

    Updated state after clicking Register button to remove username and password from screen upon successful registration

--- src/components/NewUserPage.jsx
+++ src/components/NewUserPage.jsx
@@ -19,6 +19,9 @@ const NewUserPage = () => {
     }
 
     setMessage('User successfully registered');
+    setNewUsername('');
+    setNewPassword('');
+    setConfirmPassword('');
   };
 
   return (
