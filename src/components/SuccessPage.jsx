1b267d219512ccbb09afa9b5c002fdd612de4c0a
William Tucker <william.tucker29@gmail.com>
2024-08-05 12:30:33 -0500

    Created Success page. Basic html that says user logged in successfully

---
+++ src/components/SuccessPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SuccessPage = () => {
+  return (
+    <div className="success-container">
+      <h1>Success!</h1>
+      <p>You have successfully logged in!</p>
+    </div>
+  );
+};
+
+export default SuccessPage;
